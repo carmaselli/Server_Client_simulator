864cf88ee1b6aeab4e2f69128308def8bdab52b5
Joaquin Gaytan <jgaytan@itba.edu.ar>
2018-09-09 12:50:46 -0300

    Cambios en clase fsmparser

--- TP6_EDA/fsmparser.cpp
+++ TP6_EDA/fsmparser.cpp
@@ -3,12 +3,13 @@ using namespace std;
 
 
 fsmparser::
-fsmparser()
+fsmparser(char* str2parse_)
 {
 	error = false; // no hay error
 	host = "NULL";
 	path = "NULL";
-	currState = WAIT_FOR_END_STATE; // estado inicial
+	currState = WAIT_FOR_GET_STATE; // estado inicial
+	str2parse = str2parse_;
 
 }
 
@@ -34,6 +35,39 @@ getPath(void)
 	return path;
 }
 
+char * fsmparser::
+getString(void)
+{
+	return str2parse;
+}
+
+/*
+setError()
+setter del error
+*/
+void fsmparser::
+setError(void)
+{
+	error = true;
+}
+/*
+setHost()
+setter de la variable string host
+*/
+void fsmparser::
+setHost(char* p2token_)
+{
+	string s(p2token_);
+	host = s;
+}
+
+void fsmparser::
+setPath(char * p2token_)
+{
+	string s(p2token_);
+	path = s;
+}
+
 /*
 * eventGen()
 * método generador de eventos de la fsm
@@ -52,12 +86,12 @@ eventGen(char* pointer)
 			ev.currentEv = GET_EVENT;
 		}
 
-		else if (strcmp((const char *)ev.token, " HTTP/1.1 1310Host"))
+		else if (strcmp((const char *)ev.token, " HTTP/1.1 \r\nHost"))
 		{
 			ev.currentEv = HTTP_STRING_EVENT;
 		}
 
-		else if (strcmp((const char *)ev.token, "13101310"))
+		else if (strcmp((const char *)ev.token, "\r\n\r\n"))
 		{
 			ev.currentEv = DOUBLECRLF_EVENT;
 		}
@@ -91,3 +125,59 @@ cycleFSM(event_t ev)
 	currState = (FSM_STATES)fsmArray[currState][ev.currentEv].nextState; // escribo siguiente estado
 }
 
+/*
+ parse()
+ método que corre la FSM
+*/
+bool fsmparser::
+parse(void)
+{
+	while ( (currState != END_STATE) && (error = false)) // mientras no llegué al final, y no hubo error
+	{
+		cycleFSM(eventGen(str2parse)); // ciclo la fsm
+	}
+
+	return error;
+}
+
+/********** RUTINAS DE ACCION DE LA FSM *********/
+
+void fsmError(void* pointer)
+{
+	fsmparser* p2obj = (fsmparser*) pointer;
+	p2obj->setError();
+}
+
+void savePath(void* pointer)
+{
+	fsmparser* p2obj = (fsmparser*)pointer;
+	char* p2token = strtok(p2obj->getString(), SPACE);
+
+	if (p2token != NULL)
+	{
+		p2obj->setPath(p2token); // guardo el path en la variable indicada
+		
+	}
+
+	else
+	{
+		p2obj->setError(); // hubo error en el parseo
+	}
+}
+
+void saveHost(void* pointer)
+{
+	fsmparser* p2obj = (fsmparser*)pointer;
+	char* p2token = strtok(p2obj->getString(), SPACE);
+
+	if (p2token != NULL)
+	{
+		p2obj->setHost(p2token); // guardo el host en la variable indicada
+	}
+
+	else
+	{
+		p2obj->setError(); // hubo error en el parseo
+	}
+
+}
--- TP6_EDA/fsmparser.h
+++ TP6_EDA/fsmparser.h
@@ -23,26 +23,23 @@ typedef struct
 typedef enum 
 {
 	GET_EVENT,
-	SPACE_EVENT,
 	HTTP_STRING_EVENT,
 	DOUBLECRLF_EVENT,
 	OTHER_EVENT
 
 }FSM_EVENTS;
 
-#define FSM_EV_COUNT 5
+#define FSM_EV_COUNT 4
 
 typedef enum
 {
 	WAIT_FOR_GET_STATE,
-	SAVE_PATH_STATE,
 	WAIT_FOR_HOST_STATE,
-	SAVE_HOST_STATE,
 	WAIT_FOR_END_STATE,
 	END_STATE
 }FSM_STATES;
 
-#define FSM_ST_COUNT 5
+#define FSM_ST_COUNT 4
 
 typedef void(*actrout_t)(void*);
 
@@ -59,32 +56,39 @@ class fsmparser
 {
 public:
 
-	fsmparser();
+	fsmparser(char* str2parse_);
 	~fsmparser();
-	bool parse(char* str2parse);
+	bool parse(void);
+
 	string getHost(void);
 	string getPath(void);
+	char* getString(void);
+	bool getError();
+
+	void setError(void);
+	void setHost(char* p2token_);
+	void setPath(char* p2token_);
+
 
 
 private:
 
 	string host;
 	string path;
+	char* str2parse;
 	FSM_STATES currState;
 	bool error;
 
 	event_t eventGen(char* pointer); // devuelve el evento
-	bool getError();
+
 	void cycleFSM(event_t ev);
 
 	const cells_t fsmArray[FSM_ST_COUNT][FSM_EV_COUNT]
-	{ // GET	SPACE	HTTPSTRING		DOUBLECRLF  OTHER_EVENT
-		{NULL,SAVE_PATH_STATE},{fsmError,END_STATE},{fsmError,END_STATE},{fsmError,END_STATE},{}, //WAIT_FOR_GET_STATE
-		{fsmError,END_STATE},{},{},{},{}, //SAVE_PATH_STATE
-		{},{},{},{},{}, //WAIT_FOR_HOST_STATE
-		{},{},{},{},{}, //SAVE_HOST_STATE
-		{},{},{},{},{}, //WAIT_FOR_END_STATE
-		{},{},{},{},{},//END_STATE
+	{ // GET	HTTPSTRING		DOUBLECRLF  OTHER_EVENT
+		{{savePath,WAIT_FOR_HOST_STATE},{fsmError,END_STATE},{fsmError,END_STATE},{fsmError, END_STATE}}, //WAIT_FOR_GET_STATE
+		{{fsmError,END_STATE},{saveHost,WAIT_FOR_END_STATE},{fsmError,END_STATE},{fsmError,END_STATE}}, //WAIT_FOR_HOST_STATE
+		{{fsmError,END_STATE},{fsmError,END_STATE},{NULL,END_STATE},{fsmError,END_STATE}}, //WAIT_FOR_END_STATE
+		{{NULL,END_STATE},{NULL,END_STATE},{NULL,END_STATE},{NULL,END_STATE}},//END_STATE
 	};
 
 
